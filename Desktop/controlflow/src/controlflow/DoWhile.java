3a476e5492c5b446d1734976e231f523a65a18af
Karthikeyananywhere <129921213+Karthikeyananywhere@users.noreply.github.com>
2023-04-14 13:02:50 +0530

    ControlFlow Statements

---
+++ Desktop/controlflow/src/controlflow/DoWhile.java
@@ -0,0 +1,15 @@
+package controlflow;
+
+public class DoWhile {
+	
+	public static void main(String[] args) {
+		
+		char ch='A';
+		do {
+			
+			System.out.println(ch++);
+			
+		}while(ch<=90);
+	}
+
+}
---
+++ Desktop/controlflow/src/controlflow/ForLoop.java
@@ -0,0 +1,17 @@
+package controlflow;
+
+public class ForLoop {
+	
+	public static void main(String[] args) {
+		for (char i = 'A'; i <=90; i++) {
+			
+			System.out.println(i);
+		}
+		
+		
+		
+	}
+	
+	
+
+}
